3accd4670b1257262e6a6c5e2ecb1a6d96fc53f5
Moazzam Abdullah Khan <moazzam2k@yahoo.com>
2018-11-17 19:55:22 +0100

    Site updated: 2018-11-17 19:55:22

---
+++ 2018/11/12/From-blockchain-to-ecosystem/supply-demand-1.js
@@ -0,0 +1,17 @@
+// Create characters
+	// Basic person
+		// Seller
+		// Buyer
+
+// Create stage
+	// Rope
+	// Flag
+
+// Create interactive buttons
+	// Buy
+	// Sell
+
+
+document.getElementById('buy-button-1').onclick = function() {
+	d3.select('#colorbox').style('background-color', "#000");
+}
